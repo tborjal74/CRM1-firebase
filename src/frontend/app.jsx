107cf4b8051dced9c1255f243ec542ccf4abdcd2
tborjal74 <tborjal@byupathway.edu>
2025-09-17 22:13:14 +0800

    configured package.js

--- src/frontend/app.jsx
+++ src/frontend/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -10,3 +10,13 @@ function App() {
 }
 
 export default App;
+
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <div>Hello CRM1 Frontend!</div>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
