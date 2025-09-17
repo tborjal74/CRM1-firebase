6a524edc6c6283eeda851484710c2ec55d2614d4
tborjal74 <tborjal@byupathway.edu>
2025-09-17 20:34:43 +0800

    configured API connection

--- src/backend/app.js
+++ src/backend/app.js
@@ -23,10 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
-const express = require('express');
 const customerRoutes = require('./routes/customerRoutes');
-const admin = require('firebase-admin');
-const serviceAccount = require('')
 
 app.use(express.json());
 app.use('/api/customers', customerRoutes);
