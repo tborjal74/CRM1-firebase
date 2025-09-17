ffde6649cea9d79e42449518d5dae97470c07656
tborjal74 <tborjal@byupathway.edu>
2025-09-17 21:09:53 +0800

    backend configuration

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require ('swagger-jsdoc');
 
-// Path to your service account key JSON file
+// Path to service account key JSON file
 const serviceAccount = require(path.join(__dirname, '../../crm1-e85dc-firebase-adminsdk-fbsvc-1a9ad4e13e.json'));
 
 // Initialize Firebase Admin SDK
@@ -19,14 +19,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
 	res.json({ message: 'Backend is working and connected to Firebase!' });
 });
-app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
 });
-const customerRoutes = require('./routes/customerRoutes');
 
 const swaggerOptions = {
   definition: {
@@ -37,9 +33,18 @@ const swaggerOptions = {
       description: 'API documentation for CRM1 backend',
     },
   },
-  apis: ['./src/backend/routes/*.js'], // Path to your route files
+  apis: ['./src/backend/routes/*.js'], // Path to route files
 };
+
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-app.use(express.json());
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/customers', customerRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
+
+app.use(express.json());
